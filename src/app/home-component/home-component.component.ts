6723bcac462898088864837362da44d49dfd9423
Jegajeeth Manikandan <Jegajeeth.Manikandan@nafinc.com>
2023-06-16 14:59:14 +0530

    fix(recipe): style of recipe page changed

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder} from '@angular/forms';
 
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-component',
@@ -11,38 +12,39 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 })
 export class HomeComponentComponent {
 
-  constructor(private fb: FormBuilder,){}
+  constructor(private fb: FormBuilder,private router: Router){}
 
   showRecipes: boolean = false;
 
-  recipeList: any = [
-    {
-      id: 52,
-      foodImg:
-        'https://cdn2.stylecraze.com/wp-content/uploads/2015/06/1.-Low-Fat-Tarragon-Chicken-Salad.jpg.webp',
-      name: 'Low-Fat Tarragon Chicken Salad',
-      instruction: [
-        'Add the chicken breast cubes, salt, and black pepper to a pot of boiling water.',
-        'Cook it for 15-20 minutes.',
-        'In the meantime, slice the dried cranberries and toss them into a bowl.',
-        'Add yogurt, lime juice, sliced red onions, chopped celery, salt, tarragon, and freshly ground black pepper to the bowl and mix well.',
-        'Take the cooked chicken breast cubes out and toss them in the bowl. Your low-fat chicken tarragon salad is ready to eat!',
-      ],
-      ingredients: [
-        'chicken breast cubes',
-        ' dried cranberries',
-        'low-fat yogurt',
-        'lime juice',
-        'red onions',
-        'celery',
-        'tarragon',
-        'Salt',
-        'black pepper',
-      ],
-      preperationTime: 120,
-      servingSize: 3,
-    },
-  ];
+  recipeList: any = []
+  // [
+  //   {
+  //     id: 52,
+  //     foodImg:
+  //       'https://cdn2.stylecraze.com/wp-content/uploads/2015/06/1.-Low-Fat-Tarragon-Chicken-Salad.jpg.webp',
+  //     name: 'Low-Fat Tarragon Chicken Salad',
+  //     instruction: [
+  //       'Add the chicken breast cubes, salt, and black pepper to a pot of boiling water.',
+  //       'Cook it for 15-20 minutes.',
+  //       'In the meantime, slice the dried cranberries and toss them into a bowl.',
+  //       'Add yogurt, lime juice, sliced red onions, chopped celery, salt, tarragon, and freshly ground black pepper to the bowl and mix well.',
+  //       'Take the cooked chicken breast cubes out and toss them in the bowl. Your low-fat chicken tarragon salad is ready to eat!',
+  //     ],
+  //     ingredients: [
+  //       'chicken breast cubes',
+  //       ' dried cranberries',
+  //       'low-fat yogurt',
+  //       'lime juice',
+  //       'red onions',
+  //       'celery',
+  //       'tarragon',
+  //       'Salt',
+  //       'black pepper',
+  //     ],
+  //     preperationTime: 120,
+  //     servingSize: 3,
+  //   },
+  // ];
 
 
   ingredientForm = this.fb.group({
@@ -55,13 +57,13 @@ export class HomeComponentComponent {
   }
 
   redirectToRecipe(event: any){
-    console.log(event.target);
+    const id = event.target.id
+    this.router.navigate([`/recipe/${id}`]);
     
   }
 
   get ingredient() {
     return this.ingredientForm.get('ingredient')
   }
-
 
 }
--- src/app/recipes-component/recipes-component.component.ts
+++ src/app/recipes-component/recipes-component.component.ts
@@ -7,7 +7,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./recipes-component.component.css'],
 })
 export class RecipesComponentComponent {
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   recipeList: any = [
     {
